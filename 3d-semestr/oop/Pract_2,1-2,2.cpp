2ea8ad5c7e1720b53bfad313e37c4cb8ee7511af
Prost444 <elazareva1979@gmail.com>
2023-09-23 20:52:38 +0300

    complete

--- 3d-semestr/oop/Pract_2,1-2,2.cpp
+++ 3d-semestr/oop/Pract_2,1-2,2.cpp
@@ -141,7 +141,7 @@ public:
 
 ostream& operator<<(ostream& out, const Statistics& s) 
 {
-    out << "побед: " << s.num_wins << ", ничьич: " << s.num_draws << ", поражений: " << s.num_losses << ", очков: " << s.score;
+    out << "wins: " << s.num_wins << ", draws: " << s.num_draws << ", loses: " << s.num_losses << ", score: " << s.score;
     return out;
 }
 
@@ -166,16 +166,91 @@ bool TeamsFilter (Statistics t, int threshold)
     return t.num_wins > threshold;
 }
 
+
+// Функция для поиска элементов по ключу
+template<class K, class V>
+vector<V> findValuesByKey(multimap<K, V> m, K key) 
+{
+    vector<V> res;
+    for(typename multimap<K, V>::iterator it = m.begin(); it != m.end(); it++)
+        if (it->first == key)
+            res.push_back(it->second);
+    if (res.size())
+        return res;
+    throw MapKeyExeption("Element with this key does not exist.");
+}
+
+// Функция для поиска ключей по значению
+template<class K, class V>
+vector<K> findKeysByValue(multimap<K, V> m, V value) 
+{
+    vector<K> keys;
+    for (typename multimap<K, V>::iterator it = m.begin(); it != m.end(); it++)
+        if (it->second == value)
+            keys.push_back(it->first);
+    return keys;
+}
+
+//Пишем свое добавление элемента просто по приколу
+template<class K, class V>
+typename multimap<K, V>::iterator addItem(multimap<K, V>& m,  K key, V value) 
+{
+    return m.insert(pair(key, value));
+}
+
+//Вывод с помощью итератора
+template<class K, class V>
+ostream& operator << (ostream& ustream, multimap<K, V> m)
+{
+    for (typename multimap<K, V>::iterator it = m.begin(); it != m.end(); it++)
+        ustream << "multimap[" << it->first << "]: " << it->second << ";\n";
+    return ustream;
+}
+
+//Фильтр
+template<class K, class V, class T>
+multimap<K, V> filter(multimap<K, V> m, bool (*predicate)(V, T), T threshold)
+{
+    multimap<K, V> res;
+    for (typename multimap<K, V>::iterator it = m.begin(); it != m.end(); it++)
+        if (predicate(it->second, threshold))
+            addItem(res, it->first, it->second);
+    return res;
+}
+
+//Все встречающиеся в дереве элементы
+template<class K, class V>
+vector<V> getUniqueValues(multimap<K, V> m)
+{
+    vector<V> vals;
+    for (typename multimap<K, V>::iterator it = m.begin(); it != m.end(); it++)
+    {
+        V cur = it->second;
+        bool flag = true;
+        for (int i = 0; i < vals.size(); i++)
+        {
+            if (vals[i] == cur)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            vals.push_back(it->second);
+    }
+    return vals;
+}
+
 int main()
 {
     map<string, Statistics> teams;
     addItem(teams, string("Lions City1"), Statistics(10, 5, 3, 30));
-    addItem( teams, string("Tigers City2"), Statistics(8, 7, 2, 29));
-    addItem( teams, string("Bears City3"), Statistics(12, 3, 2, 35));
-    addItem( teams, string("Eagles City4"), Statistics(8, 7, 2, 29));
-    addItem( teams, string("Wolves City5"), Statistics(10, 5, 3, 30));
-    addItem( teams, string("Dolphins City6"), Statistics(6, 6, 6, 24));
-    addItem( teams, string("Panthers City7"), Statistics(10, 5, 3, 30));
+    addItem(teams, string("Tigers City2"), Statistics(8, 7, 2, 29));
+    addItem(teams, string("Bears City3"), Statistics(12, 3, 2, 35));
+    addItem(teams, string("Eagles City4"), Statistics(8, 7, 2, 29));
+    addItem(teams, string("Wolves City5"), Statistics(10, 5, 3, 30));
+    addItem(teams, string("Dolphins City6"), Statistics(6, 6, 6, 24));
+    addItem(teams, string("Panthers City7"), Statistics(10, 5, 3, 30));
     cout << "\nSport teams:\n";
     cout << teams;
 
@@ -187,7 +262,26 @@ int main()
     cout << "\nFilterd tree with num_wins > 8:\n" << filter(teams, TeamsFilter, 8);
 
 
+    multimap<string, Statistics> multi_teams;
 
-    
+    addItem(multi_teams, string("Lions City1"), Statistics(10, 5, 3, 30));
+    addItem(multi_teams, string("Tigers City2"), Statistics(8, 7, 2, 29));
+    addItem(multi_teams, string("Bears City3"), Statistics(12, 3, 2, 35));
+    addItem(multi_teams, string("Eagles City4"), Statistics(8, 7, 2, 29));
+    addItem(multi_teams, string("Wolves City5"), Statistics(10, 5, 3, 30));
+    addItem(multi_teams, string("Dolphins City6"), Statistics(6, 6, 6, 24));
+    addItem(multi_teams, string("Panthers City7"), Statistics(10, 5, 3, 30));
+    addItem(multi_teams, string("Panthers City7"), Statistics(10, 5, 3, 30));
+    addItem(multi_teams, string("Panthers City7"), Statistics(9, 4, 6, 20));
+
+    cout << "\nMultimap tree of sport teams:\n" << multi_teams << endl;
+
+    cout << "\nFinding Panthers City7:\n" << findValuesByKey(multi_teams, string("Panthers City7"));
+    cout << "\nFinding Statistics(10, 5, 3, 30):\n" << findKeysByValue(multi_teams, Statistics(10, 5, 3, 30)) << "\n";
+
+    cout << "\nUnique statistics:\n" << getUniqueValues(multi_teams);
+
+    cout << "\nFilterd tree with num_wins > 8:\n" << filter(multi_teams, TeamsFilter, 8);
+
     return 0;
 }
